564551b77cb8411d99bf2bbd62e96566b5794083
Andrew Gomes <andrewgomes1328@gmail.com>
2023-08-15 21:00:18 -0300

    up

--- resources/views/formulario.blade.php
+++ resources/views/formulario.blade.php
@@ -7,26 +7,25 @@
     <form action="/series" method="POST" enctype="multipart/form-data">
         @csrf
         <div class="form-group">
-            <label for="name">Nome:</label>
+            <label for="name"><strong>Nome:</strong></label>
             <input type="text" name="name" id="name" class="form-control" placeholder="Digite o nome do anime, serie ou filme..." required autocomplete="off">
         </div>
         <div class="form-group">
-            <label for="descricao">Descrição:</label>
+            <label for="descricao"><strong>Descrição:</strong></label>
             <textarea name="descricao" id="descricao" placeholder="Digite aqui a descrição..." class="form-control"  required autocomplete="off"></textarea>
         </div>
         <div class="form-group">
-            <label for="link">Link:</label>
+            <label for="link"><strong>Link:</strong></label>
             <input type="text" name="link" id="link" placeholder="Digite o link..." class="form-control" required autocomplete="off">
         </div>
         <div class="form-group">
-            <label for="image">Imagem:</label>
+            <label for="image"><strong>Imagem:</strong></label>
             <input type="file" name="image" id="image" class="form-control">
         </div>
-        <div class="form-group">
-            <label for="items">Status:</label>
+        <div class="form-group checkbox">
+            <label for="items"><strong>Status:</strong></label>
             <input type="checkbox" name="items[]" value="Finalizado">Finalizado
             <input type="checkbox" name="items[]" value="Em andamento">Em andamento
-            <label for="items"></label>
         </div>
         <input type="submit" value="Cadastrar" class="btn btn-primary">
     </form>
