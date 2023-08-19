6aaf79100c43787dba4bdd86503c6ba5511444f1
Andrew Gomes <andrewgomes1328@gmail.com>
2023-08-19 19:55:15 -0300

    updates

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -20,10 +20,24 @@ class VideoController extends Controller
 
     public function store(Request $request)
     {
+
+        $regras = [
+            'nome' => ['required', 'min:5'],
+            'link' => ['required', 'url']
+        ];
+
+        $mensagensErro = [
+            'nome.required' => 'É obrigatório conter um título!',
+            'nome.min' => 'É obrigatório conter no mínimo 5 caracteres!',
+            'link.required' => 'É obrigatório conter uma url!',
+            'link.url' => 'Digite uma url válida!'
+        ];
+
         $video = new Video();
         $video->nome = $request->nome;
         $video->link = $request->link;
 
+        $request->validate($regras, $mensagensErro);
         $video->save();
 
         return redirect('/videos',)->with('msg', 'Vídeo adicionado com sucesso!');
