8a9f6e426c2e82b5765e75a1f3bcab6fce96c576
Andrew Gomes <andrewgomes1328@gmail.com>
2023-09-22 23:05:53 -0300

    consumindo minha api feita em laravel (poucos recursos, fiz em 15 minutos

--- app/Http/Controllers/API/SerieController.php
+++ app/Http/Controllers/API/SerieController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SerieRequest;
 use App\Models\Serie;
 use App\Services\SerieService;
-use Illuminate\Http\Request;
+use Exception;
 
 class SerieController extends Controller
 {
@@ -21,14 +22,43 @@ class SerieController extends Controller
         return Serie::all();
     }
 
-    public function store(Request $request)
+    public function store(SerieRequest $request)
     {
-        return response()->json(Serie::create($request->all()), 201);
+        $data = $request->all();
+        return $this->service->create($data);
     }
 
     public function show($id)
     {
-        $serie = $this->service->getById($id);
-        return $serie;
+        try {
+            $serie = $this->service->getById($id);
+            if ($serie === null) {
+                return response()->json(['error' => 'Série não encontrada'], 404);
+            }
+            return $serie;
+        } catch (Exception $erro) {
+            return response()->json(['error' => $erro->getMessage()], 500);
+        }
+    }
+
+    public function destroy($id)
+    {
+        $this->service->delete($id);       
+        return response()->json(['message' => 'Série excluída com sucesso'], 200);
+    }
+
+    public function edit($id, SerieRequest $request)
+    {
+        $data = $request->All();
+
+        if (!$data) {
+            return response()->json(['error' => 'Série não encontrada'], 404);
+        }
+
+        $data = $request->validated();
+    
+        $this->service->update($id, $data);
+
+        return response()->json(['message' => 'Série atualizada com sucesso'], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\SerieController;
+use App\Models\Serie;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +23,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/series', [SerieController::class, 'index']);
 Route::post('/series', [SerieController::class, 'store']);
 Route::get('/series/{id}', [SerieController::class, 'show']);
+Route::delete('/series/{id}', [SerieController::class, 'destroy']);
+Route::put('/series/edit/{id}', [SerieController::class, 'edit']);
